c908c56a0a2f4cf22d5246e5ad78e18d4e9bc7b8
davidkep <david@rotinaj.org>
2017-03-21 20:03:22 -0700

    Add inline math and change example genes

--- shiny-app/global.R
+++ shiny-app/global.R
@@ -4,13 +4,16 @@ library(shiny)
 gene.summary.data <- readRDS("data-cache/gene-summary-data.rds")
 
 selected.example.genes <- c(
+    "DIABLO", #cor = 0.17
+    "PPP1CC", #cor = 0.07
+    "PPIA", # cor = -0.02
+    "UFC1", # cor = -0.04
     "FN3K", # cor = 0.89
-    "BTF3", # cor = 0.38
-    "TBCD", # cor = 0.03
-    "PRDX5", # cor = 0
-    "UFC1", # cor = -0.02
-    "SRSF1", # cor = -0.41
     "HNRNPH2" # cor = -0.75
+    # "BTF3", # cor = 0.38
+    # "TBCD", # cor = 0.03
+    # "PRDX5", # cor = 0
+    # "SRSF1", # cor = -0.41
 )
 
 selected.example.genes.with.cor <- with(gene.summary.data, {
